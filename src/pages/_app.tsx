d0ae23fe5626b5c72329ea34c5a2e6aadccef5f7
aguenaro <rodrigoaguena@gmail.com>
2021-09-23 18:19:56 -0300

    🎉  first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from "next/app";
+// import { ChakraProvider } from "chakra-ui/react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
