0af2e1ccaab802888ed0d39b586081e3c8f8ec27
aguenaro <rodrigoaguena@gmail.com>
2021-10-05 22:50:40 -0300

    ⚡️ turn header dynamic

--- src/components/Header/NavigationMenu.tsx
+++ src/components/Header/NavigationMenu.tsx
@@ -1,38 +1,72 @@
-import { HStack, Text, Button } from '@chakra-ui/react';
+import { MdNotifications } from 'react-icons/md';
+
+import { HStack, Text, Button, Icon } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { AddMenu } from './AddMenu';
+import { ProfileMenu } from './ProfileMenu';
 
 export const NavigationMenu = () => {
-  return (
-    <HStack spacing={12}>
-      <Link href="/">
-        <Text fontSize="xl" color="white">
-          home
-        </Text>
-      </Link>
-      <Link href="/docs">
+  const { route } = useRouter();
+
+  // const externalPaths = ['/', '/signin', '/signup'];
+  const internalPaths = ['/boards', '/boards/[boardId]'];
+
+  console.log(route);
+
+  const ExternalMenu = () => {
+    return (
+      <>
+        <Link href="/">
+          <Text fontSize="xl" color="white">
+            home
+          </Text>
+        </Link>
+        <Link href="/docs">
+          <Text fontSize="xl" color="white">
+            api
+          </Text>
+        </Link>
+        <Link href="/signin">
+          <Text fontSize="xl" color="white">
+            sign in
+          </Text>
+        </Link>
+        <Link href="/signup">
+          <Button
+            borderRadius={15}
+            size="md"
+            bg="teal.200"
+            boxShadow="dark-lg"
+            color="black"
+            style={{ boxShadow: '0px 0px 20px 4px #4BCFEE' }}
+            _hover={{ color: 'white' }}
+            zIndex="1"
+          >
+            SIGN UP
+          </Button>
+        </Link>{' '}
+      </>
+    );
+  };
+
+  const InternalMenu = () => {
+    return (
+      <>
         <Text fontSize="xl" color="white">
           api
         </Text>
-      </Link>
-      <Link href="signin">
-        <Text fontSize="xl" color="white">
-          login
-        </Text>
-      </Link>
-      <Link href="/signup">
-        <Button
-          borderRadius={15}
-          size="md"
-          bg="teal.200"
-          boxShadow="dark-lg"
-          color="black"
-          style={{ boxShadow: '0px 0px 20px 4px #4BCFEE' }}
-          _hover={{ color: 'white' }}
-          zIndex="1"
-        >
-          SIGN UP
-        </Button>
-      </Link>
+        <Icon as={MdNotifications} color="white" w={7} h={7} />
+        <AddMenu />
+        <ProfileMenu />
+      </>
+    );
+  };
+
+  return (
+    <HStack spacing={12}>
+      {internalPaths.includes(route) ? <InternalMenu /> : <ExternalMenu />}
     </HStack>
   );
 };
