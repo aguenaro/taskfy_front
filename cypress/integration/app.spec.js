f3f5a1fd28bdb39d7bf1d80ffa5af168f46a2e11
Eduardo Santos <santos.eduardothomaz@gmail.com>
2021-10-13 23:43:36 -0300

    :white_check_mark: Navigation tests added

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,4 +1,4 @@
-const server = 'http://localhost:3000';
+const server = Cypress.env('server');
 
 describe('Navigation', () => {
   it('Navigate from home to signup', () => {
@@ -6,5 +6,26 @@ describe('Navigation', () => {
     cy.contains('SIGN UP').click();
 
     cy.url().should('include', '/signup');
+  });
+
+  it('Navigate from landing page card to signup', () => {
+    cy.visit(server);
+    cy.contains('improve my tech productivity').click();
+
+    cy.url().should('include', '/signup');
+  });
+
+  it('Navigate from singup to signin', () => {
+    cy.visit(server);
+    cy.contains('sign in').click();
+
+    cy.url().should('include', '/signin');
+  });
+
+  it('Navigate from singup to home', () => {
+    cy.visit(server);
+    cy.get('[alt="Logo taskfy"]').click();
+
+    cy.url().should('eq', `${server}`);
   });
 });
