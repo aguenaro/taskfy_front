27d7b8a3941c906b3566bbbcf62db9c5a6123ef2
aguenaro <rodrigoaguena@gmail.com>
2021-11-02 19:31:15 -0300

    ✅  add navigation test on sign in page

--- cypress/integration/signin.spec.ts
+++ cypress/integration/signin.spec.ts
@@ -1,6 +1,20 @@
-describe('Sign Up', () => {
+describe('Sign In', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/signin');
+  });
+
+  it('Navigate to home', () => {
+    cy.get('[alt="logo taskfy branca"]').click();
+
+    cy.wait(10000);
+    cy.url().should('include', '/');
+  });
+
+  it('Navigate to sign up', () => {
+    cy.contains('Crie').click();
+
+    cy.wait(10000);
+    cy.url().should('include', '/signup');
   });
 
   it('Invalid all sign in form', () => {
