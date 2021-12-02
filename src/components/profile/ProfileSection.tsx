6ae492269041bd7ecaf5469c15192cceb5c809ab
aguenaro <rodrigoaguena@gmail.com>
2021-12-02 14:13:46 -0300

    🐛  fix the SSR

--- src/components/profile/ProfileSection.tsx
+++ src/components/profile/ProfileSection.tsx
@@ -10,20 +10,22 @@ export const ProfileSection = () => {
       </Text>
       <Divider />
       <Flex align="center" w="100%" p="50px 100px">
-        <Flex align="center">
-          <Avatar
-            name={`${user.firstName} ${user.lastName}`}
-            w="150px"
-            h="150px"
-            mr={10}
-          />
-          <Flex direction="column">
-            <Text color="white" fontSize="2xl">
-              {user.firstName} {user.lastName}
-            </Text>
-            <Text color="gray.300">{user.username}</Text>
+        {user.firstName && (
+          <Flex align="center">
+            <Avatar
+              name={`${user.firstName} ${user.lastName}`}
+              w="150px"
+              h="150px"
+              mr={10}
+            />
+            <Flex direction="column">
+              <Text color="white" fontSize="2xl">
+                {user.firstName} {user.lastName}
+              </Text>
+              <Text color="gray.300">{user.username}</Text>
+            </Flex>
           </Flex>
-        </Flex>
+        )}
       </Flex>
     </>
   );
